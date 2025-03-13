b18290c7dda27dc5a3f630392d0cd24327627ad2
Momox <119704058+momoox@users.noreply.github.com>
2025-03-13 16:53:13 +0100

    Add files via upload

---
+++ srcs/requirements/mariadb/tools/mariadb-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+	#--------------mariadb start--------------#
+	service mariadb start # start mariadb
+	sleep 2 # wait for mariadb to start
+
+	#--------------mariadb config--------------#
+	# Create database if not exists
+	mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+
+	# Create user if not exists
+	mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+	# Grant privileges to user
+	mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
+
+	# Flush privileges to apply changes
+	mariadb -e "FLUSH PRIVILEGES;"
+
+	#--------------mariadb restart--------------#
+	# Shutdown mariadb to restart with new config
+	mariadb-admin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+		
+	mysqld --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+
+	touch /tmp/started
